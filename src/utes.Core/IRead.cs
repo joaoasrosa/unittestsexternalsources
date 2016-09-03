679a19c98ed76f8f428c133f477745c036f23e49
joaoasrosa <joaoasrosa@gmail.com>
2016-09-03 14:34:40 +0100

    First commit. Add the solution and basic infrastructure

---
+++ src/utes.Core/IRead.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace utes.Core
+{
+    /// <summary>
+    /// Interface to read from the external data source
+    /// </summary>
+    public interface IRead<T>
+    {
+        /// <summary>
+        /// Method to read from the external data source
+        /// </summary>
+        /// <returns>The collection of objects of <typeparamref name="T"/>T</returns>
+        IEnumerable<T> Read();
+    }
+}
