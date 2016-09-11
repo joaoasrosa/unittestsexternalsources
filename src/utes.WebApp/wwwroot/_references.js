844b3d2f764e4ca052a7a1d68a456eb211e9a414
joaoasrosa <joaoasrosa@gmail.com>
2016-09-11 17:04:53 +0100

    Add the web app to the solution.

---
+++ src/utes.WebApp/wwwroot/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="js/site.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/react/react.js" />
+/// <reference path="lib/react/react-dom.js" />
