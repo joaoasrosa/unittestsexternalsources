ae2516e8836b3520091466da545c63850dcfb0fe
joaoasrosa <joaoasrosa@gmail.com>
2016-09-28 20:19:11 +0100

    Add a dummy test

---
+++ tests/utes.WebApp.Tests/Controllers/AssemblyController.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace utes.WebApp.Tests.Controllers
+{
+    /// <summary>
+    /// Class to hold the tests for the Assembly controller
+    /// </summary>
+    public class AssemblyController
+    {
+        [Fact]
+        public void Dummy()
+        {
+            // Arrange
+            var assemblyController = new WebApp.Controllers.AssemblyController(null, null);
+            
+            // Act
+
+            // Assert
+            Assert.False(false);
+        }
+    }
+}
