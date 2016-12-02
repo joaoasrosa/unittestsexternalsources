de7eecc7b0422105ad6304bb67d635d5dcf53241
joaoasrosa <joaoasrosa@gmail.com>
2016-12-02 11:54:55 +0000

    Change the server configuration to kestrel only, removing IIS dependency

--- src/utes.WebApp/Program.cs
+++ src/utes.WebApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace utes.WebApp
 {
@@ -7,10 +8,18 @@ namespace utes.WebApp
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                })
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
